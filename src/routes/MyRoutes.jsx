c24569e78434f262a02e78df80777b5c82b17a26
yardentamir <yarden.tamir@walla.co.il>
2023-10-17 16:05:12 +0300

    fix for deploy

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -1,11 +1,10 @@
-import HomePage from "pages/Home/HomePage";
-import MoviePage from "pages/Movie/MoviePage";
 import { Route, Routes } from "react-router-dom";
+import withDynamicImport from "services/withDynamicImport";
 import { DETAILS_PATH, HOME_PATH } from "./routes.constants";
 
 export default function MyRoutes() {
-  // const HomePage = withDynamicImport(() => import('pages/Home/HomePage'));
-  // const MoviePage = withDynamicImport(() => import('pages/Movie/MoviePage'));
+  const HomePage = withDynamicImport(() => import('pages/Home/HomePage'));
+  const MoviePage = withDynamicImport(() => import('pages/Movie/MoviePage'));
 
   return (
     <Routes>
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
+
+const projectRootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: "/src",
-      components: "/src/components",
-      routes: "/src/routes",
-      pages: "/src/pages",
-      services: "/src/services",
-      config: "/src/config",
-      features: "/src/features",
+      src: path.resolve(projectRootDir, "src"),
+      components: path.resolve(projectRootDir, "src/components"),
+      routes: path.resolve(projectRootDir, "src/routes"),
+      pages: path.resolve(projectRootDir, "src/pages"),
+      services: path.resolve(projectRootDir, "src/services"),
+      config: path.resolve(projectRootDir, "src/config"),
+      features: path.resolve(projectRootDir, "src/features"),
     },
   },
 });
